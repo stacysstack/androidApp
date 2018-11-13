ef657e14efbb0a9abdf5d8bff81f5e27ca3f2f3c
Stacy <alme0056@umn.edu>
2018-11-13 01:33:47 -0600

    refining accounts

---
+++ app/src/main/java/com/example/csci5115_project/CreditCards.java
@@ -0,0 +1,10 @@
+//  looking for some code to properly format credit card data!!!!!!!!!!
+//
+//  public class CreditCards {
+//    package com.example.csci5115_project;
+//
+//        private int id;
+//        private String title;
+//        private String shortdesc;
+//
+//  }
