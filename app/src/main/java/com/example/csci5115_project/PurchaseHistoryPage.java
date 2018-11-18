bc765a110e2f0d884d056fa42ef7b12b841ce978
Stacy <alme0056@umn.edu>
2018-11-17 18:05:31 -0600

    added login page

--- app/src/main/java/com/example/csci5115_project/PurchaseHistoryPage.java
+++ app/src/main/java/com/example/csci5115_project/PurchaseHistoryPage.java
@@ -35,16 +35,16 @@ public class PurchaseHistoryPage extends AppCompatActivity
         setContentView(R.layout.activity_purchase_history);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
-            }
-        });
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
